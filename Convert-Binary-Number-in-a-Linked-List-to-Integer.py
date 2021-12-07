0aa2360ad0f25359234f46e780e904e68aeb553c
San Ayush <41052723+San1357@users.noreply.github.com>
2021-12-07 14:58:01 +0530

    Create Convert-Binary-Number-in-a-Linked-List-to-Integer.py

---
+++ Convert-Binary-Number-in-a-Linked-List-to-Integer.py
@@ -0,0 +1,16 @@
+
+
+# problem no. : 1290
+# problem name: Convert Binary Number in a Linked List to Integer
+#time :O(N) 
+#space : O(1)
+
+# code : 
+
+class Solution:
+    def getDecimalValue(self, head: ListNode) -> int:
+        num = head.val
+        while head.next:
+            num = (num << 1) | head.next.val
+            head = head.next
+        return num
