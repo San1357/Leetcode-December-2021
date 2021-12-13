30e9e49d17cfe6daba5294f01f24bd029537e3ee
San Ayush <41052723+San1357@users.noreply.github.com>
2021-12-13 12:18:04 +0530

    Create consecutive-character.py

---
+++ consecutive-character.py
@@ -0,0 +1,24 @@
+
+
+#Problem  name : Consecutive Character 
+#Problem no. : 1446
+#time : O(n)
+#Space : O(1)
+
+#Code :
+
+class Solution:
+    def maxPower(self, s: str) -> int:
+        count = 0
+        max_count = 0
+        previous = None
+        for c in s:
+            if c == previous:
+                # if same as previous one, increase the count
+                count += 1
+            else:
+                # else, reset the count
+                previous = c
+                count = 1
+            max_count = max(max_count, count)
+        return max_count
