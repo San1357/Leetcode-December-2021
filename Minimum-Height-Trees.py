c958a7cf8d9f695e468a0075c570b4f5b852821e
San Ayush <41052723+San1357@users.noreply.github.com>
2021-12-16 10:33:10 +0530

    Create Minimum-Height-Trees.py

---
+++ Minimum-Height-Trees.py
@@ -0,0 +1,50 @@
+
+
+
+#Problem no.: 310
+#Problem name.: Minimum Height Trees
+
+#time: O(∣V∣)
+#space: O(∣V∣)
+
+
+#Code :
+class Solution:
+    def findMinHeightTrees(self, n: int, edges: List[List[int]]) -> List[int]:
+
+        # base cases
+        if n <= 2:
+            return [i for i in range(n)]
+
+        
+        neighbors = [set() for i in range(n)]
+        for start, end in edges:
+            neighbors[start].add(end)
+            neighbors[end].add(start)
+
+        
+        leaves = []
+        for i in range(n):
+            if len(neighbors[i]) == 1:
+                leaves.append(i)
+
+        
+        remaining_nodes = n
+        while remaining_nodes > 2:
+            remaining_nodes -= len(leaves)
+            new_leaves = []
+            
+            while leaves:
+                leaf = leaves.pop()
+                
+                neighbor = neighbors[leaf].pop()
+                
+                neighbors[neighbor].remove(leaf)
+                if len(neighbors[neighbor]) == 1:
+                    new_leaves.append(neighbor)
+
+            
+            leaves = new_leaves
+
+        
+        return leaves
