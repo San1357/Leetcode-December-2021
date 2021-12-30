a7adceb23d14d9693f9b453220544d20ee03695e
San Ayush <41052723+San1357@users.noreply.github.com>
2021-12-30 13:41:56 +0530

    Create 1015-Smallest-Integer-Divisible-by-K.py

---
+++ 1015-Smallest-Integer-Divisible-by-K.py
@@ -0,0 +1,12 @@
+
+#Time Complexity : O(K)
+#Space Complexity : O(1)
+
+class Solution:
+    def smallestRepunitDivByK(self, K: int) -> int:
+        remainder = 0
+        for length_N in range(1,K+1):
+            remainder = (remainder*10+1) % K
+            if remainder == 0:
+                return length_N
+        return -1
